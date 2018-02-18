96e654fa64f2e0d6c3ec5feae90530730c437fba
kamatari <8.incidents.sr.9@gmail.com>
2018-02-18 17:29:11 +0900

    add colorscheme setup

--- setup.sh
+++ setup.sh
@@ -6,3 +6,10 @@ for file in ${DOT_FILES[@]}
 do
     ln -s $HOME/proj/dotfiles/$file $HOME/$file
 done
+
+cd $HOME/proj/dotfiles
+git submodule init
+git submodule update
+mkdir -p $HOME/.vim/colors
+mv colorscheme/molokai/colors/molokai.vim $HOME/.vim/colors/
+
